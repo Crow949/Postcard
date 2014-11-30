6337c4202cf3f206d350076a8a3ad4d86340f6f1
Charles Payne <charlespayne@Chuckie.local>
2014-11-29 19:25:31 -0800

    nameLabel functionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
-    @IBOutlet weak var mailButton: UIButton!
     
+    @IBOutlet weak var mailButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,11 +29,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
-        // Code will evaluate whne we press the button
-        //  Adding comments to test git commit
+        // Code will evaluate when we press the button
+        // Adding comments to test git commit
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
